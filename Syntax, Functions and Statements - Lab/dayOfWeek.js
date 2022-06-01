190c451e1055d6dc3e1a7071db2b118075d0f0a1
Yasen Kurtev <yasenuni@abv.bg>
2022-06-01 11:36:02 +0300

    Added completed tasks and their descriptions from the current lesson.

---
+++ Syntax, Functions and Statements - Lab/dayOfWeek.js
@@ -0,0 +1,13 @@
+function dayOfWeek(input) {
+
+    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    if (days.includes(input)) {
+        console.log(days.indexOf(input) + 1);
+    } else {
+        console.log('error');
+    }
+
+}
+
+dayOfWeek('Friday')
