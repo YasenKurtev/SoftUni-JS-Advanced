a9c820980b0b34320ad36e1200fd33efaebaf601
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-06-02 18:35:52 +0300

    Deleted directory.

--- Syntax, Functions and Statements/fruit.js
+++
@@ -1,9 +0,0 @@
-function fruit(fruit, weightG, priceKG) {
-
-    let weightKG = weightG / 1000;
-
-    console.log(`I need $${(weightKG * priceKG).toFixed(2)} to buy ${weightKG.toFixed(2)} kilograms ${fruit}.`);
-
-}
-
-fruit('orange', 2500, 1.80)
